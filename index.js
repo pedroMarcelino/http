8059af2b3a03dfc05b59160564dafe5c6b29ba99
pedroMarcelino <38365875+pedroMarcelino@users.noreply.github.com>
2019-12-04 17:29:35 -0300

    a

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-$(function(){
+$(function () {
 
-    $('.item-nav').on('click', function(){
+    $('.item-nav').on('click', function () {
         var newId = "#";
         newId += $(this).attr('id');
+        newId += 'Title';
 
-        $('.title').attr('class')
-    }); 
+        $('.title').attr('class', 'hidden');
+        $(newId).attr('class', 'display-1 title animated bounceIn');
+
+        $('.content').attr('class', 'hidden');
+
+        var newClass = '#';
+        newClass += $(this).attr('id');
+        newClass += 'Container';
+
+        $(newClass).attr('class', 'container content animated fadeInLeft');
+
+    });
 
 });
